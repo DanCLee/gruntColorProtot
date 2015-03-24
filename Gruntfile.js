e9cd61eaa3094696730baa4b145e745bcd75c1a2
Daniel Lee <dan.clee1218@gmail.com>
2015-03-24 00:12:51 -0700

    add Grunt Watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,28 @@ module.exports = function(grunt) {
 grunt.initConfig({
   concat: {
 	    js: {
-	      src: ['js/bootstrap.js', 'js/Gruntfile.js', 'js/jquery.addDot.js', 'js/jquery.js', 'js/jqueryUI.js', 'js/p5.dom.js', 'js/p5.js', 'js/sketch.js', 'tinycolor.js' ],
-	      dest: 'build/built.js',
+	      src: ['js/bootstrap.js', 'js/jquery.addDot.js', 'js/jquery.js', 'js/jqueryUI.js', 'js/p5.dom.js', 'js/p5.js', 'js/sketch.js', 'tinycolor.js' ],
+	      dest: 'build/js/built.js',
 	    },
   },
+
+	watch: {
+		  	js: {
+		    	files: ['js/**/*.js'],
+		    	tasks: ['concat:js'],
+
+			 },
+			 css: {
+		    	files: ['css/**/*.css'],
+		    	tasks: ['concat:css'],
+			 },
+	},  
 });
 
 
 
 grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.registerTask('default', ['concat', 'watch']);
 
-}
+};
